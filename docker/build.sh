fc0378d6b433d5e960176d95fcaadaa52e2b40ed
Alex Black <alex@alexblack.ca>
2016-08-04 16:10:22 -0700

    error if missing env variables

--- docker/build.sh
+++ docker/build.sh
@@ -3,4 +3,15 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 cd ..
+
+if [ -z $AWS_ACCESS_KEY_ID ]; then
+  echo 'Missing env setting AWS_ACCESS_KEY_ID'
+  exit 1
+fi
+
+if [ -z $AWS_SECRET_ACCESS_KEY ]; then
+  echo 'Missing env setting AWS_SECRET_ACCESS_KEY'
+  exit 1
+fi
+
 docker build -t ardupilot-build --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY .
